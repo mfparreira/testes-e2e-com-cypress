d849cd7bcc08df29642d05c383509874239e7db3
Marcelo Parreira <marcelo.parreira@mfmti.com.br>
2025-03-20 15:25:49 -0300

    Create e2e tests with cypress

---
+++ cypress.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://notes-serverless-app.com', // Altere para a URL do seu projeto
+    env: {
+      viewportWidthBreakpoint: 768,
+    },
+    chromeWebSecurity: false,
+    defaultCommandTimeout: 20000, // Tempo de espera padrão para comandos (em ms)
+    setupNodeEvents(on, config) {
+      // Configurações adicionais podem ser adicionadas aqui
+    },
+ 
+  },
+});
---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,10 @@
+describe('Login', () => {
+    it('successfully logs in', () => {
+
+        cy.guiLogin()
+
+        cy.contains('a', 'Create a new note').should('be.visible')
+    })
+})
+
+
