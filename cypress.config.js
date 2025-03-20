a77277bb4f2a535c1bf704bc390f20bdad8c9aa0
Marcelo Parreira <marcelo.parreira@mfmti.com.br>
2025-03-20 16:49:07 -0300

    adicionando cypress cloud2

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,13 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  chromeWebSecurity: false,
+  defaultCommandTimeout: 20000, // Tempo de espera padrão para comandos (em ms)
   e2e: {
     baseUrl: 'https://notes-serverless-app.com', // Altere para a URL do seu projeto
     env: {
       viewportWidthBreakpoint: 768,
     },
-    chromeWebSecurity: false,
-    defaultCommandTimeout: 20000, // Tempo de espera padrão para comandos (em ms)
-    // setupNodeEvents(on, config) {
-    //   // Configurações adicionais podem ser adicionadas aqui
-    // },
   },
   projectId: 'z4q8x8',
 });
